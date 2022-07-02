e0b25c561d675c99a8c1a6c07a2773a876fab55e
darrell404 <darrell029@gmail.com>
2022-07-02 16:46:05 +0100

    Add nearest upcoming booking on dashboard and sort bookings by date & time on fetch

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,8 +1,27 @@
 import useFetchUserInfo from './hooks/useFetchUserInfo'
+import useFetchBookings from './hooks/useFetchBookings'
 import Loading from './Loading'
+import { useEffect, useState } from 'react'
 
 export default function HomePage() {
   const { userData, userDataLoading } = useFetchUserInfo()
+  const { bookings } = useFetchBookings()
+  const [upcomingBooking, setUpcomingBooking] = useState({})
+  const now = new Date().toLocaleString()
+
+  useEffect(() => {
+    if(bookings.length !== 0) {
+      for (let booking of bookings) {
+        console.log(booking)
+        let bookingTime = new Date(`${booking.booking_date} ${booking.booking_time}`).toLocaleString()
+        if (bookingTime > now) {
+          console.log(bookingTime)
+          setUpcomingBooking(booking)
+          break
+        }
+      }
+    }
+  }, [bookings])
 
 return (
   <div className="dashboard col-span-4 p-8">
@@ -10,8 +29,15 @@ return (
     <>
       <h1 className='text-3xl text-center'>Welcome, {userData.first_name}!</h1>
       <p>
-        Book appointments with us now
+        Book appointments with us now {console.log(bookings)}
       </p>
+      <h2>Your upcoming booking</h2>
+      <div>
+        {Object.keys(upcomingBooking).length === 0 ?
+        <p>You have no upcoming bookings</p> :
+        <p>{upcomingBooking.booking_date} {upcomingBooking.booking_time}</p>
+        }
+      </div>
     </>
     }
     </div>
